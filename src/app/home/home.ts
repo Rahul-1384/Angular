d5da1fcdc21b42028a37af76b4ed911c13dbdc62
Rahul Rajput <156775643+rahul1384r@users.noreply.github.com>
2025-08-10 22:37:52 +0530

    feat: routing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Greetings } from '../components/greetings/greetings';
+import { Counter } from '../components/counter/counter';
+import { Todos } from '../todos/todos';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Greetings, Counter, Todos, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
+  homeMessage = signal('Parent data')
 
+  keyUpHandler(event: KeyboardEvent){
+    console.log(`The key ${event.key} pressed`);
+  }
 }
